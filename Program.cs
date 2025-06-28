743c40ae7ead205164bc884c440951f0d44191c3
chaithat.suk <chaithat.suk@bigc.co.th>
2025-06-28 17:57:12 +1000

    add files

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using clean_netcore;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
--- README.md
+++
@@ -1,2 +0,0 @@
-# clean-netcore
-Clean .Net Core
